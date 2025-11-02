b9417dfef2c4ff79dd6b738de654f53cb85c3ebb
Harshit Srivastava <srivastavah818@gmail.com>
2025-11-02 07:08:15 +0530

    Add new routes for WorkLab, Community, Messages, Bookmarks, and Settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import WorkLab from "./pages/WorkLab";
+import Community from "./pages/Community";
+import Messages from "./pages/Messages";
+import Bookmarks from "./pages/Bookmarks";
+import Settings from "./pages/Settings";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,6 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/WorkLab" element={<WorkLab />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
     </Router>
